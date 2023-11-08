5fdf712f7d8a084d1c9f129e913ca9052c14ae4c
MichalPruszynski7 <mpruszynski@hawk.iit.edu>
2023-11-07 20:29:04 -0600

    java

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init(){
+  var button = document.getElementById('entrybutton');
+
+  function showMeText(){
+  	var textbox = document.getElementById('entryinput');
+  	document.getElementById('textoutput').innerHTML = textbox.value;
+  	alert("Michal Pruszynski: " + textbox.value);
+  }
+
+  buttom.addEventListerner('click', showMeText);
+  }
+  
+window.addEventListener('load', init);
