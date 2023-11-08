c7691468519758f2b3cb78bae92efbd5b2762629
MichalPruszynski7 <mpruszynski@hawk.iit.edu>
2023-11-07 20:46:04 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function init(){
 
   function showMeText(){
   	var textbox = document.getElementById('entryinput');
-  	document.getElementById('textoutput').innerHTML = textbox.value;
+  	document.getElementById('textoutput').innerHTML =textbox.value;
   	alert("Michal Pruszynski: " + textbox.value);
   }
 
